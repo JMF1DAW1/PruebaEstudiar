2f40047eb08fec9964f192fb3f473e73dcc739ac
JMF1DAW1 <marco12853@iesmarenostrum.com>
2021-06-07 12:14:35 +0200

    He añadido el bank y el bank test. Solo tiene 1 method.

---
+++ Bank/BankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("095803b0-1d7e-44e8-babf-8072999979d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bank/BankTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // preparación del caso de prueba 
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // acción a probar
+            account.Debit(debitAmount);
+
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            double actual = account.Balance;
+
+            Assert.AreEqual(expected, actual, 0.001, "Error");
+        }
+    }
+}
